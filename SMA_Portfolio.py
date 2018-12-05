f5ae118b6ed549411d5d9d293ce5c894bbd62f59
apokrif333 <alex.ah@list.ru>
2018-12-05 16:32:15 -0500

    Home

--- SMA_Portfolio.py
+++ SMA_Portfolio.py
@@ -1,19 +1,18 @@
+from datetime import datetime
 import sys
 import os
-sys.path.append(os.path.abspath('C:/Users/Lex/PycharmProjects/Start/GitHub/My_Libs'))
-
 import pandas as pd
 import numpy as np
-import trading_lib as tl
+sys.path.insert(0, 'C:/Users/Tom/PycharmProjects/Start/GibHub/My_Libs')
 
-from datetime import datetime
+import trading_lib as tl
 
 # Variables
 newest_dates = [datetime(2000, 9, 1)]  # Стартовая дата (можно не указывать)
 oldest_dates = [datetime.now()]  # Конечная дата (можно не указывать)
 
 def_data_direct = 'exportTables'
-tickers_list = ['ARKK', 'FINX', 'FXH', 'IBUY', 'IGV', 'PSCH', 'XAR']  # Список тикеров для портфеля 'FBT', 'ITA', 'QQQ', 'FXH'
+tickers_list = ['ARKK', 'FINX', 'FBT', 'FDN', 'PSCH', 'ITA']  # Список тикеров для портфеля 'FBT', 'ITA', 'QQQ', 'FXH'
 
 sma_ticker = 'SPY'  # Тикер из которого берётся SMA для конца месяца
 sma_period = 200
@@ -21,9 +20,9 @@ sma_period = 200
 start_capital = 1_000_000
 
 download_data = 1  # Если не нужно перекачивать все инструменты False
-calc_SMA = 0 if download_data == 0 else 1
-hedge_ticker = 'IEF'  # Тикер в который порт будет уходить для хэджа
-chart_or_save = 1  # 1 вывести итоговый график, -1 сохранить файл, 0 вывести график и сохранить файл
+calc_SMA = 1 if download_data == 0 else 1
+hedge_ticker = 'TLT'  # Тикер в который порт будет уходить для хэджа
+chart_or_save = 0  # 1 вывести итоговый график, -1 сохранить файл, 0 вывести график и сохранить файл
 positions = len(tickers_list)
 
 
